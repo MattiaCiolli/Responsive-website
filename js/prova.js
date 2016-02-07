1a9aed3ccb8b5fb5fc04f0a3fd4369c1c5847249
DavidGia <gia.davide@gmail.com>
2016-02-07 22:15:31 +0100

    aggiustamento features e screenshot

--- js/prova.js
+++ js/prova.js
@@ -21,46 +21,11 @@ $(document).ready(function(){
     if ($(document).width() < 768) {
       $(".p1").css("color", "black");
       $(".p2").css("color", "black");
-      $(".p3").css("color", "black"); 
+      $(".p3").css("color", "black");
+
+      $(".imgResp").css("width", "210px"); 
     }
   });
 });
 
-/**
-$(document).ready(function(){
-  $(window).scroll(function() { // check if scroll event happened
-    if ($(document).scrollTop() > 10) { // check if user scrolled more than 50 from top of the browser window
-      $(".navbar-fixed-top").css("background-color", "#f8f8f8"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-    } else {
-      $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
-    }
-  });
-});
-
-
-$(document).ready(function(){
-  $(window).scroll(function() {
-    if ($(document).scrollTop() < 75) { 
-      $("#ph").attr("src","img/phone1.png");
-      $(".p1").css("color", "black"); 
-      $(".p2").css("color", "lightgray");
-      $(".p3").css("color", "lightgray");
-    } 
-    if ($(document).scrollTop() > 75) { 
-      $("#ph").attr("src","img/phone2.png");
-      $(".p1").css("color", "lightgray"); 
-      $(".p2").css("color", "black");
-      $(".p3").css("color", "lightgray"); 
-    }
-    if ($(document).scrollTop() > 200) { 
-      $("#ph").attr("src","img/phone3.png");
-      $(".p1").css("color", "lightgray"); 
-      $(".p2").css("color", "lightgray");
-      $(".p3").css("color", "black"); 
-    } 
-  });
-});
-
-
-*/
 
