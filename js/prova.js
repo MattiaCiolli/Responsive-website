c4d276c788585c0d990aaebbbb5adfbad7694dd6
Matteo Polsinelli <netrider91@gmail.com>
2016-02-09 12:12:28 +0100

    no message

--- js/prova.js
+++ js/prova.js
@@ -1,31 +1,29 @@
 $(document).ready(function(){
   $(window).scroll(function() {
-    if ($(document).scrollTop() < $("#p1id").height() && $(document).width() > 768 ) { 
-      $("#ph").attr("src","img/phone1.png");
-      $(".p1").css("color", "black"); 
+    if ($(document).scrollTop() < $("#p1id").height() && $(document).width() > 768 ) {
+      $("#ph").attr("src","img/appHome.png");
+      $(".p1").css("color", "black");
       $(".p2").css("color", "lightgray");
       $(".p3").css("color", "lightgray");
-    } 
-    if ($(document).scrollTop() > $("#p1id").height() && $(document).width() > 768) { 
-      $("#ph").attr("src","img/phone2.png");
-      $(".p1").css("color", "lightgray"); 
-      $(".p2").css("color", "black");
-      $(".p3").css("color", "lightgray"); 
     }
-    if ($(document).scrollTop() > $("#p1id").height()+$("#p2id").height()-10 && $(document).width() > 768) { 
-      $("#ph").attr("src","img/phone3.png");
-      $(".p1").css("color", "lightgray"); 
+    if ($(document).scrollTop() > $("#p1id").height() && $(document).width() > 768) {
+      $("#ph").attr("src","img/appFollowing.png");
+      $(".p1").css("color", "lightgray");
+      $(".p2").css("color", "black");
+      $(".p3").css("color", "lightgray");
+    }
+    if ($(document).scrollTop() > $("#p1id").height()+$("#p2id").height()-10 && $(document).width() > 768) {
+      $("#ph").attr("src","img/appAccordion.png");
+      $(".p1").css("color", "lightgray");
       $(".p2").css("color", "lightgray");
-      $(".p3").css("color", "black"); 
-    } 
+      $(".p3").css("color", "black");
+    }
     if ($(document).width() < 768) {
       $(".p1").css("color", "black");
       $(".p2").css("color", "black");
       $(".p3").css("color", "black");
 
-      $(".imgResp").css("width", "210px"); 
+      $(".imgResp").css("width", "210px");
     }
   });
 });
-
-
